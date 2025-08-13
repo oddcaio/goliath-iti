bfa328154c93fa0670140344ae1343a294456e5b
Oddcaio <caiokakalima10@gmail.com>
2025-08-13 17:46:48 -0300

    menu icon added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,27 @@
 import { Link, useNavigate } from 'react-router-dom';
-
+import React, { useState } from 'react';
 function Navbar() {
 
   const navigate = useNavigate();
-
-  const goToPage = () => {
-    navigate("/rota")
-  }
-
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <h2>Goliath</h2>
-        <button onClick={goToPage}>teste</button>
-        <p>oioi</p>
+        <h2>
+          <Link to="/" className='navbar-logo'>Goliath</Link>
+        </h2>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
