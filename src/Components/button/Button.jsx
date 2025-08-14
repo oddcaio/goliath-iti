5212fc13c589bb944581d709230888e00f9a6fa9
Oddcaio <caiokakalima10@gmail.com>
2025-08-14 15:26:42 -0300

    button added

--- src/Components/button/Button.jsx
+++ src/Components/button/Button.jsx
@@ -7,4 +7,17 @@ const SIZES = ['btn--medium', 'btn--large']
 
 export const Button = ({ children, type, onclick, buttonStyle, buttonSize }) => {
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
-}
+
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
+
+return (
+    <Link to="/" className="btn-mobile">
+        <button
+            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+            onClick={onClick}
+            type={type}
+        >
+            {children}
+        </button>
+    </Link>
+);
