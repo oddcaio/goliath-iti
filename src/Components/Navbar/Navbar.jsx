cc3bf5e09d800223795390728d1739933aa8f80e
Oddcaio <caiokakalima10@gmail.com>
2025-08-14 16:55:30 -0300

    header working

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,31 @@
 import { Link, useNavigate } from 'react-router-dom';
 import React, { useState } from 'react';
 import { Button } from '../button/Button';
+import './Navbar.css';
 function Navbar() {
 
   const navigate = useNavigate();
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true)
+
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false);
+  } else {
+    setButton(true);
+  }
+};
+
+  window.addEventListener('resize', showButton);
 
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <h2>
-          <Link to="/" className='navbar-logo'>Goliath</Link>
+          <Link to="/" className='navbar-logo'> Iti Goliath</Link>
         </h2>
         <div className="menu-icon" onClick={handleClick}>
           <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
@@ -27,11 +40,11 @@ function Navbar() {
             <Link to='/certificados' className='nav-links' onClick={closeMobileMenu}>
               Certificados
             </Link>
-            <li className='nav-item'>
-              <Link to='/estrutura' className='nav-links' onClick={closeMobileMenu}>
-                Estrutura
-              </Link>
-            </li>
+          </li>
+          <li className='nav-item'>
+            <Link to='/estrutura' className='nav-links' onClick={closeMobileMenu}>
+              Estrutura
+            </Link>
           </li>
         </ul>
         <Button buttonStyle={'btn--outline'}>VEJA TODAS APIS</Button>
