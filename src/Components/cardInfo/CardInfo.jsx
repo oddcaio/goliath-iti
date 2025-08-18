0e2b77b28dd1eb4369c467a9a7fc9571f8867828
caio medeiros <caiokakalima10@gmail.com>
2025-08-18 18:50:59 -0300

    working on certificados

---
+++ src/Components/cardInfo/CardInfo.jsx
@@ -0,0 +1,10 @@
+function CardInfo({ title, value }) {
+  return (
+    <div className="card-info">
+      <h4>{title}</h4>
+      <p>{value}</p>
+    </div>
+  );
+}
+
+export default CardInfo;
--- src/pages/Certificados.jsx
+++ src/pages/Certificados.jsx
@@ -1,5 +1,15 @@
-function Certificados() {
-  return <h1>Bem-vindo Bomba!</h1>;
+import React from 'react';
+import '../Components/cardInfo/CardInfo.css';
+
+function CardInfo({ title, value }) {
+  return (
+  <div style={{ display: 'flex', gap: '16px', flexWrap: 'wrap' }}>
+      <CardInfo title="Ativos (não expirados, não revogados)" value="15.074.068" />
+      <CardInfo title="Emitidos" value="7.449.057" />
+      <CardInfo title="Total de emissões em relação ao ano anterior" value="69,5 %" />
+      <CardInfo title="Projeção para 2025" value="11.781.441" />
+    </div>
+  );
 }
 
-export default Certificados;
+export default CardInfo;
